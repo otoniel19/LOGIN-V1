90dd96cc140b6cc802b9c5f16c01cc226cc0392c
otoniel19 <84187511+otoniel19@users.noreply.github.com>
2021-06-27 16:12:26 -0300

    update script.js

--- MENU-LOGIN/script.js
+++ MENU-LOGIN/script.js
@@ -1,17 +1,16 @@
 function checar() {
- let all = document.getElementsByName('menu')
- 
- if(all[0].checked) {
-   window.open("pages/sobre.html")
- }
- if(all[1].checked) {
-   window.open("pages/agradecimentos.html")
- }
- if(all[2].checked) {
-   window.open("pages/contato.html")
- }
+  let all = document.getElementsByName('menu')
+
+  if (all[0].checked) {
+    window.open("pages/sobre.html")
+  }
+  if (all[1].checked) {
+    window.open("pages/agradecimentos.html")
+  }
+  if (all[2].checked) {
+    window.open("pages/contato.html")
+  }
 }
 function back() {
-  window.open("")
+  window.open("https://otoniel19.github.io/LOGIN-V1/")
 }
-
